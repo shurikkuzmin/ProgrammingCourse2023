9efadb0da6c57a4258e2297af4784afb09f8ed84
Alexandr Kuzmin <shurik.kuzmin@gmail.com>
2023-10-14 09:24:50 -0400

    First commit.

---
+++ age.py
@@ -0,0 +1,10 @@
+a=3
+b=4
+c = False
+a=a+2
+if a>b:
+    print("a > b")
+    print("Hello world")
+else:
+    print("a<=b")
+
---
+++ intro.py
@@ -0,0 +1,8 @@
+a = 5
+b=-1.23
+c = "This is a text!"
+print(a+a)
+print(c+c)
+print(c.upper())
+c = c.upper()
+print(c.replace("This","That"))

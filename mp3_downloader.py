67a6e5d89f48804be9e0bea265f45a9ead2cd2dc
Alexandr Kuzmin <shurik.kuzmin@gmail.com>
2023-11-04 10:31:46 -0400

    Added pytube lib.

--- mp3_downloader.py
+++ mp3_downloader.py
@@ -1,1 +1,24 @@
 import csv
+import pytube
+
+with open('music.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    for row in csv_reader:
+        file_name = row[0] + "_" + row[1] + ".mp3"
+        file_name = file_name.replace(" ","")
+        yt = pytube.YouTube(row[2])
+        print(file_name)
+
+
+#yt = YouTube( 
+#    str(input("Enter the URL of the video you want to download: \n>> "))) 
+  
+# extract only audio 
+#video = yt.streams.filter(only_audio=True).first() 
+  
+# check for destination to save file 
+#print("Enter the destination (leave blank for current directory)") 
+#destination = str(input(">> ")) or '.'
+  
+# download the file 
+#out_file = video.download(output_path=destination) 

7b4d3f38a7a360c301b2e4fbc8493678a0d7ff45
Alexandr Kuzmin <shurik.kuzmin@gmail.com>
2023-11-18 09:58:09 -0500

    Spirograph.

--- spirograph.py
+++ spirograph.py
@@ -1,8 +1,17 @@
 import turtle
+import math
 
 screen = turtle.Screen()
 screen.setup(500, 500)
 
 t = turtle.Turtle()
+t.penup()
+t.goto(200,0)
+t.pendown()
+for my_time in range(0,720):
+    my_angle = my_time * math.pi / 180.0
+    x = int(200 * math.cos(my_angle))
+    y = int(200 * math.sin(my_angle))
+    t.goto(x,y)
 
 turtle.mainloop()

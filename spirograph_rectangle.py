8c42faca79c4cb193392cb05dbe873241ba766f1
Alexandr Kuzmin <shurik.kuzmin@gmail.com>
2023-12-01 19:54:12 -0500

    Reworking rectangle spirograph.

--- spirograph_rectangle.py
+++ spirograph_rectangle.py
@@ -7,37 +7,39 @@ screen.setup(500, 500)
 t = turtle.Turtle()
 t.speed(0)
 
-a = 50
-b = 100
-t.goto(a/2, 0)
-t.left(90)
-for my_time in range(0,500):
+#a = 150
+#b = 300
+a=20
+b=80
+
+def draw_rectangle(my_time):
+    remainder_time = my_time % (2*(a+b))
+    x, y = 0, 0
+    if remainder_time < b:
+        x = int(a/2)
+        y = int(remainder_time)- int(b/2)
+    elif remainder_time < b + a:
+        x = b + int(a/2) -int(remainder_time)
+        y = int(b/2)
+    elif remainder_time < 2*b + a:
+        x = -int(a/2)
+        y= int(1.5*b) + a - int(remainder_time)
+    else:
+        x = int(remainder_time) - 2*b - 1.5*a
+        y =-int(b/2)
+    return x,y
+
+t.goto(a/2,-b/2)
+for my_time in range(0,2000):
     my_angle = my_time * math.pi / 180.0
-#     #x = int(30 * math.cos(4.378*my_angle)) + int(200*math.cos(-my_angle))
-#     #y = int(30 * math.sin(4.378*my_angle)) + int(200*math.sin(-my_angle))
-    t.penup()
-    if t.ycor() > b/2:         
-        t.left(90)
-        t.sety(b/2)
 
-    if t.xcor() < -a/2:
-        t.left(90)
-        t.setx(-a/2)
+    x, y = draw_rectangle(3*my_time)
+    #xnew = x + int(20*math.cos(7.5*my_angle))
+    #ynew = y + int(20*math.sin(7.5*my_angle))
 
-    if t.ycor() < -b/2:
-        t.left(90)
-        t.sety(-b/2)
-    if t.xcor() > a/2:
-        t.left(90)
-        t.setx(a/2)
-    t.forward(1)
-    x, y = t.pos()
-    xnew = x + int(200*math.cos(-my_angle))
-    ynew = y + int(200*math.sin(-my_angle))
+    xnew = x + int(200*math.cos(-4.1*my_angle))
+    ynew = y + int(200*math.sin(-4.1*my_angle))
+    
     t.goto(xnew,ynew)
-    t.pendown()
-    t.dot(3)
-    t.penup()
-    t.goto(x,y)
 
 turtle.mainloop()
